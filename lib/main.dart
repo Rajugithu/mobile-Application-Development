35ccdb0851568c72d825239a84f4cb2045ab4492
Rajugithu <mujahidr319@gmail.com>
2024-09-12 15:33:33 +0500

    Assignment1

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'MaximumBid.dart';  // Import the separate class from bidding_page.dart
 
 void main() {
-  runApp(MyApp());  // Entry point of the app
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bidding App',   // App title
+      title: 'Bidding App',
       theme: ThemeData(
         primarySwatch: Colors.blue,  // App theme
       ),
-      home: MaximumBid(),  // Call the MaximumBid class from bidding_page.dart
+      home: MaximumBid(),
     );
   }
 }
